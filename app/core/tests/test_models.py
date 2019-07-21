d506c870287edb7053c931578c20965f96461e7b
nikolasnelson <nelson.nikolas@gmail.com>
2019-07-21 19:27:52 +0100

    adding user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,7 +8,7 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'test@email.com'
         password = 'Testpass123'
-        user = get_user_model().objects.craate_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
